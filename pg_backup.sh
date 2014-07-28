c6b69ddf55324fe9ba732e6e856693d59e7dc213
linruhe <linruhe@hotmail.com>
2014-07-28 15:35:10 +0800

    Update pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,1 +1,1 @@
- /home/lihaihe/workspace/Asia_Power/runtime/pgsql/bin/pg_dump -h 127.0.0.1 -p 65432 -U openerp asia >  /home/lihaihe/workspace/Asia_Power/backup/asia_$(date +%Y%m%d).backup
+ /home/reportserver/reportserver/server/runtime/pgsql/bin/pg_dump -h 127.0.0.1 -p 5432 -U openerp asia_power_report_production >  /home/reportserver/reportserver/server/backup/asia_power_report_production_$(date +%Y%m%d).backup
